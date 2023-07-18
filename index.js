c5eaad7c92f23284ed61c71ce65e6d6c17299ff3
Liam Potter <liam@liampotter.co.uk>
2023-07-19 00:46:14 +0100

    - Use custom-media-element - Support changing the src

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Web Components: Extending Native Elements, A working example
-
-import CustomVideoElement from 'custom-video-element';
+import { CustomVideoElement } from 'custom-media-element';
 import Hls from 'hls.js';
 
 class HLSVideoElement extends CustomVideoElement {
@@ -9,64 +7,38 @@ class HLSVideoElement extends CustomVideoElement {
   }
 
   get src() {
-    // Use the attribute value as the source of truth.
-    // No need to store it in two places.
-    // This avoids needing a to read the attribute initially and update the src.
-    return this.getAttribute('src');
+    return super.src;
   }
 
-  set src(val) {
-    // If being set by attributeChangedCallback,
-    // dont' cause an infinite loop
-    if (val !== this.src) {
-      this.setAttribute('src', val);
-    }
+  set src(src) {
+    super.src = src;
+    if (this.#hlsPlayer) this.#hlsPlayer.loadSource(this.src);
   }
+
+  #hlsPlayer = null;
 
   load() {
     if (Hls.isSupported()) {
-      var hls = new Hls({
-        // Kind of like preload metadata, but causes spinner.
-        // autoStartLoad: false,
-
+      this.#hlsPlayer = new Hls({
         // Mimic the media element with an Infinity duration
         // for live streams
-        liveDurationInfinity: true
+        liveDurationInfinity: true,
       });
 
-      hls.loadSource(this.src);
-      hls.attachMedia(this.nativeEl);
+      this.#hlsPlayer.loadSource(this.src);
+      this.#hlsPlayer.attachMedia(this.nativeEl);
     } else if (this.nativeEl.canPlayType('application/vnd.apple.mpegurl')) {
       this.nativeEl.src = this.src;
     }
   }
 
-  // play() {
-  //   if (this.readyState === 0 && this.networkState < 2) {
-  //     this.load();
-  //     this.hls.on(Hls.Events.MANIFEST_PARSED,function() {
-  //     video.play();
-  //
-  //     return this.nativeEl.play();
-  //   }
-  // }
-
   connectedCallback() {
     this.load();
-
-    // Not preloading might require faking the play() promise
-    // so that you can call play(), call load() within that
-    // But wait until MANIFEST_PARSED to actually call play()
-    // on the nativeEl.
-    // if (this.preload === 'auto') {
-    //   this.load();
-    // }
   }
 }
 
-if (!window.customElements.get('hls-video')) {
-  window.customElements.define('hls-video', HLSVideoElement);
-  window.HLSVideoElement = HLSVideoElement;
+if (globalThis.customElements && !globalThis.customElements.get('hls-video')) {
+  globalThis.customElements.define('hls-video', HLSVideoElement);
 }
 
 export default HLSVideoElement;
