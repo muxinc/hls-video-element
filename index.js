3e767e88a8f1de689e7b9762cfafc526ccc0d634
Wesley Luyten <me@wesleyluyten.com>
2023-07-19 15:34:14 -0500

    Revert "fix: fix loading new src"
    This reverts commit 6aaa580765366df2df61ed1474be6585840a5173.

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
+// Web Components: Extending Native Elements, A working example
+
 import { CustomVideoElement } from 'custom-media-element';
 import Hls from 'hls.js';
 
 class HLSVideoElement extends CustomVideoElement {
-
-  attributeChangedCallback(attrName, oldValue, newValue) {
-    if (attrName !== 'src') {
-      super.attributeChangedCallback(attrName, oldValue, newValue);
-    }
-
-    if (attrName === 'src' && oldValue != newValue) {
-      this.load();
-    }
+  constructor() {
+    super();
   }
 
-  #destroy() {
-    if (this.api) {
-      this.api.detachMedia();
-      this.api.destroy();
-      this.api = null;
+  get src() {
+    // Use the attribute value as the source of truth.
+    // No need to store it in two places.
+    // This avoids needing a to read the attribute initially and update the src.
+    return this.getAttribute('src');
+  }
+
+  set src(val) {
+    // If being set by attributeChangedCallback,
+    // dont' cause an infinite loop
+    if (val !== this.src) {
+      this.setAttribute('src', val);
     }
   }
 
   load() {
-    this.#destroy();
-
-    if (!this.src) {
-      return;
-    }
-
     if (Hls.isSupported()) {
-
       var hls = new Hls({
+        // Kind of like preload metadata, but causes spinner.
+        // autoStartLoad: false,
+
         // Mimic the media element with an Infinity duration
         // for live streams
         liveDurationInfinity: true
@@ -38,16 +36,37 @@ class HLSVideoElement extends CustomVideoElement {
 
       hls.loadSource(this.src);
       hls.attachMedia(this.nativeEl);
-
     } else if (this.nativeEl.canPlayType('application/vnd.apple.mpegurl')) {
-
       this.nativeEl.src = this.src;
     }
   }
+
+  // play() {
+  //   if (this.readyState === 0 && this.networkState < 2) {
+  //     this.load();
+  //     this.hls.on(Hls.Events.MANIFEST_PARSED,function() {
+  //     video.play();
+  //
+  //     return this.nativeEl.play();
+  //   }
+  // }
+
+  connectedCallback() {
+    this.load();
+
+    // Not preloading might require faking the play() promise
+    // so that you can call play(), call load() within that
+    // But wait until MANIFEST_PARSED to actually call play()
+    // on the nativeEl.
+    // if (this.preload === 'auto') {
+    //   this.load();
+    // }
+  }
 }
 
-if (globalThis.customElements && !globalThis.customElements.get('hls-video')) {
-  globalThis.customElements.define('hls-video', HLSVideoElement);
+if (!window.customElements.get('hls-video')) {
+  window.customElements.define('hls-video', HLSVideoElement);
+  window.HLSVideoElement = HLSVideoElement;
 }
 
 export default HLSVideoElement;
