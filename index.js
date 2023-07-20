d1078a63023bfd512ceefc62474e2f3f181d7c21
Wesley Luyten <me@wesleyluyten.com>
2023-07-19 20:05:09 -0500

    feat: add preload (#28)

--- index.js
+++ index.js
@@ -35,7 +35,45 @@ class HLSVideoElement extends CustomVideoElement {
         liveDurationInfinity: true
       });
 
-      this.api.loadSource(this.src);
+      // Set up preload
+
+      switch (this.nativeEl.preload) {
+        case 'none': {
+          // when preload is none, load the source on first play
+          const loadSourceOnPlay = () => this.api.loadSource(this.src);
+          this.nativeEl.addEventListener('play', loadSourceOnPlay, {
+            once: true,
+          });
+          this.api.on(Hls.Events.DESTROYING, () => {
+            this.nativeEl.removeEventListener('play', loadSourceOnPlay);
+          });
+          break;
+        }
+        case 'metadata': {
+          const originalLength = this.api.config.maxBufferLength;
+          const originalSize = this.api.config.maxBufferSize;
+          // load the least amount of data possible
+          this.api.config.maxBufferLength = 1;
+          this.api.config.maxBufferSize = 1;
+          // and once a user has player, allow for it to load data as normal
+          const increaseBufferOnPlay = () => {
+            this.api.config.maxBufferLength = originalLength;
+            this.api.config.maxBufferSize = originalSize;
+          };
+          this.nativeEl.addEventListener('play', increaseBufferOnPlay, {
+            once: true,
+          });
+          this.api.on(Hls.Events.DESTROYING, () => {
+            this.nativeEl.removeEventListener('play', increaseBufferOnPlay);
+          });
+          this.api.loadSource(this.src);
+          break;
+        }
+        default:
+          // load source immediately for any other preload value
+          this.api.loadSource(this.src);
+      }
+
       this.api.attachMedia(this.nativeEl);
 
     } else if (this.nativeEl.canPlayType('application/vnd.apple.mpegurl')) {
